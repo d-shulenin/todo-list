d628243c6d654835bd0ffeba79818496e7764e56
Шуленин Даниил <dannnikk70787@yandex.ru>
2023-03-14 00:23:16 +0300

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import List from "./components/List/List";
 import add from "./assets/icons/add.svg";
 
 function App() {
-  const [theme, setTheme] = useState<"light" | "dark">("dark");
+  const [theme, setTheme] = useState<"light" | "dark">("light");
   return (
     <div className="app" data-theme={theme}>
       <h1 className="app__logo">Daily Todos</h1>
